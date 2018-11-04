e2cb84ed46ef0f9ed8b6a3297c5236e853d60f91
AbdulMalikShakir <AbdulMalik@shakirConsulting.com>
2018-11-03 17:17:53 -0700

    Update RunBSeRIGPublisher.bat

---
+++ RunBSeRIGPublisher.bat
@@ -0,0 +1,3 @@
+java -Xmx1024M -jar org.hl7.fhir.igpublisher.jar -simplifier "C:\Users\AbdulMalik\Documents\GitHub\bser" -destination "C:\Users\AbdulMalik\Documents\GitHub\bser\output" -npm-name org.hl7.us.bser -canonical http://hl7.org/fhir/us/bser -license CC0-1.0 -dependsOn hl7.fhir.us.core#1.1.0
+
+pause
